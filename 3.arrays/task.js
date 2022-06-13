fd34e234fc99d384ff3b6cfd68c6e567be525676
arrowls <babuyoff@gmail.com>
2022-06-13 09:51:17 +0300

    task 3 improved

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,10 +1,5 @@
 function compareArrays(arr1, arr2) {
-  if (arr2.length > arr1.length) {
-    [arr1, arr2] = [arr2, arr1];
-  }
-  return arr1.every((current, index) => {
-    return arr2[index] === current;
-  });
+  return arr1.length === arr2.length && arr1.every((item, index) => item == arr2[index]);
 }
 
 function advancedFilter(arr) {
